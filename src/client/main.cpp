bcbf36fc9a36188d39f6a8b5601ae5fae87bb64b
Maël <ext-mael.archenault@aalto.fi>
2025-10-03 08:39:54 +0200

    fixed some errors on diagram + adding gamerules

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -15,12 +15,8 @@ using namespace state;
 
 int main(int argc,char* argv[])
 {
-    Example example;
-    ExampleA exampleA;
-    exampleA.setX(53);
-    example.setA(exampleA);
 
-    cout << "It works !" << endl;
+    cout << "Code compiled" << endl;
 
     return 0;
 }
