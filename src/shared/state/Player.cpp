9c9d9c9ae26ad5082669f71971399dea082c0c80
ViMorel <victormorel45190@gmail.com>
2025-10-10 12:04:09 +0200

    player methods

--- src/shared/state/Player.cpp
+++ src/shared/state/Player.cpp
@@ -2,25 +2,56 @@
 
 namespace state {
 
-Player::Player() {
+Player::Player(const int id):id(id) {
+    int money = 0;
+    std::vector<Tribe*> tribes;
 }
 
 void Player::gather_free_units(int tribe_id) {
+    int curr_tribe = Player::tribes.size();
+    for(int i=0; i<curr_tribe; i++){
+        if(tribe_id == tribes[i]->id){
+            tribes[i]->gather_free_units();
+            return;
+        }
+    }
 }
 
 int Player::get_free_units_number(int tribe_id) {
+    int curr_tribe = Player::tribes.size();
+    for(int i=0; i<curr_tribe; i++){
+        if(tribe_id == tribes[i]->id){
+            return tribes[i]->get_free_units_number();
+        }
+    }
 }
 
-std::vector<int> Player::get_conquest_prices(int tribe_id) {
+std::vector<std::vector<int>> Player::get_conquest_prices(int tribe_id) {
+    int curr_tribe = Player::tribes.size();
+    for(int i=0; i<curr_tribe; i++){
+        if(tribe_id == tribes[i]->id){
+            return tribes[i]->get_conquest_prices();
+        }
+    }
 }
 
-void Player::roll_dice_for_bonus_units(int tribe_id) {
+void Player::redeploy_units(int tribe_id, int area_id, int n_added_units) {
+    int curr_tribe = Player::tribes.size();
+    for(int i=0; i<curr_tribe; i++){
+        if(tribe_id == tribes[i]->id){
+            tribes[i]->redeploy_units(area_id, n_added_units);
+            return;
+        }
+    }
 }
 
-void Player::deploy_units(int tribe_id, int area_id, int n_added_units) {
+void Player::conquer(int attacking_tribe_id, Area& attacked_area, int n_units, int dice_units){
+    int curr_tribe = Player::tribes.size();
+    for(int i=0; i<curr_tribe; i++){
+        if(attacking_tribe_id == tribes[i]->id){
+            tribes[i]->conquer(attacked_area, n_units, dice_units);
+            return;
+        }
+    }
 }
-
-void Player::conquer(int attacking_tribe_id, Area& attacked_area, int n_units) {
-}
-
 }
