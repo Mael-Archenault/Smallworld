a24b4bd8e6452f307635cde710ed5368dc2255fe
Maël <ext-mael.archenault@aalto.fi>
2025-10-16 10:46:27 +0200

    added some dummy test files for map

---
+++ test/shared/test_map.cpp
@@ -0,0 +1,19 @@
+#include <boost/test/unit_test.hpp>
+
+#include "state/Map.h"
+
+
+BOOST_AUTO_TEST_CASE(TestStaticAssert)
+{
+  BOOST_CHECK(1);
+}
+
+BOOST_AUTO_TEST_CASE(TestExemple)
+{
+  {
+    // state::Map map;
+    // map.load_from_json("data.json");
+  }
+}
+
+/* vim: set sw=2 sts=2 et : */
