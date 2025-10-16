036b14e5f15b7b13d9d993ebf8cb95cf1bb93ff2
ViMorel <victormorel45190@gmail.com>
2025-10-16 09:28:43 +0200

    all method of Player

--- src/shared/state/Player.cpp
+++ src/shared/state/Player.cpp
@@ -8,8 +8,8 @@ Player::Player(const int id):id(id) {
 }
 
 void Player::gather_free_units(int tribe_id) {
-    int curr_tribe = Player::tribes.size();
-    for(int i=0; i<curr_tribe; i++){
+    int number_of_owned_tribes = Player::tribes.size();
+    for(int i=0; i<number_of_owned_tribes-1; i++){
         if(tribe_id == tribes[i]->id){
             tribes[i]->gather_free_units();
             return;
@@ -18,8 +18,8 @@ void Player::gather_free_units(int tribe_id) {
 }
 
 int Player::get_free_units_number(int tribe_id) {
-    int curr_tribe = Player::tribes.size();
-    for(int i=0; i<curr_tribe; i++){
+    int number_of_owned_tribes = Player::tribes.size();
+    for(int i=0; i<number_of_owned_tribes-1; i++){
         if(tribe_id == tribes[i]->id){
             return tribes[i]->get_free_units_number();
         }
@@ -27,8 +27,8 @@ int Player::get_free_units_number(int tribe_id) {
 }
 
 std::vector<std::vector<int>> Player::get_conquest_prices(int tribe_id) {
-    int curr_tribe = Player::tribes.size();
-    for(int i=0; i<curr_tribe; i++){
+    int number_of_owned_tribes = Player::tribes.size();
+    for(int i=0; i<number_of_owned_tribes-1; i++){
         if(tribe_id == tribes[i]->id){
             return tribes[i]->get_conquest_prices();
         }
@@ -36,8 +36,8 @@ std::vector<std::vector<int>> Player::get_conquest_prices(int tribe_id) {
 }
 
 void Player::redeploy_units(int tribe_id, int area_id, int n_added_units) {
-    int curr_tribe = Player::tribes.size();
-    for(int i=0; i<curr_tribe; i++){
+    int number_of_owned_tribes = Player::tribes.size();
+    for(int i=0; i<number_of_owned_tribes-1; i++){
         if(tribe_id == tribes[i]->id){
             tribes[i]->redeploy_units(area_id, n_added_units);
             return;
@@ -46,12 +46,31 @@ void Player::redeploy_units(int tribe_id, int area_id, int n_added_units) {
 }
 
 void Player::conquer(int attacking_tribe_id, Area& attacked_area, int n_units, int dice_units){
-    int curr_tribe = Player::tribes.size();
-    for(int i=0; i<curr_tribe; i++){
+    int number_of_owned_tribes = Player::tribes.size();
+    for(int i=0; i<number_of_owned_tribes-1; i++){
         if(attacking_tribe_id == tribes[i]->id){
             tribes[i]->conquer(attacked_area, n_units, dice_units);
             return;
         }
     }
 }
+
+void Player::get_rewards(){
+    int number_of_tribes = tribes.size();
+    for(int i=0;i<number_of_tribes;i++){
+        money += tribes[i]->get_rewards();
+    }
+}
+
+void Player::go_in_decline(){
+    int number_of_tribes = tribes.size();
+    for(int i=0;i<number_of_tribes-1;i++){
+        tribes[i]->go_in_decline();
+    }
+}
+
+void Player::set_active_tribe(Tribe& tribe){
+    tribes.push_back(&tribe);
+}
+
 }
