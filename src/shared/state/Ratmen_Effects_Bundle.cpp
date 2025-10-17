447164bf57da3cbbc84e5b2c7d465b04cc9717bd
philalis <julien.metzelard@ensea.fr>
2025-10-17 09:25:58 +0000

    Fixing Effects_Bundle and all children classes, also in dia

---
+++ src/shared/state/Ratmen_Effects_Bundle.cpp
@@ -0,0 +1,13 @@
+//
+// Created by vboxuser on 10/17/25.
+//
+//
+// Created by vboxuser on 10/17/25.
+//
+#include "Ratmen_Effects_Bundle.h"
+
+state::Ratmen_Effects_Bundle::Ratmen_Effects_Bundle() = default;
+void state::Ratmen_Effects_Bundle::apply_first_round_effect (){}
+void state::Ratmen_Effects_Bundle::apply_conquest_effect (){}
+void state::Ratmen_Effects_Bundle::apply_lose_effect (){}
+int state::Ratmen_Effects_Bundle::get_bonus_rewards (state::Area& area){return 0;}
