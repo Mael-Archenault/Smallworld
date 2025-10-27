4c19ae76f69c4c2243f67f59eb1a8bbc6b8ccde4
Maël Archenault <mael.archenault@gmail.com>
2025-10-27 17:54:03 +0100

    adding units_renderer

---
+++ src/shared/renderer/Units_Renderer.cpp
@@ -0,0 +1,44 @@
+#include "Units_Renderer.h"
+
+
+namespace renderer {
+
+    bool Units_Renderer::pawn_spritesheet_loaded = false;
+
+    sf::Texture Units_Renderer::pawn_spritesheet;
+   
+    Units_Renderer::Units_Renderer(){
+    };
+
+
+    void Units_Renderer::set_sprite (std::string species_name) {
+        if (!pawn_spritesheet_loaded) {
+            if (!pawn_spritesheet.loadFromFile("/home/mael-archenault/Desktop/Smallworld/src/graphic_resources/pawn_spritesheet_255x255.png")) {
+                throw std::runtime_error("Units_Renderer::set_species: Failed to load pawn spritesheet");
+            }
+            pawn_spritesheet_loaded = true;
+        }
+
+        int frameWidth = 255;
+        int frameHeight = 255;
+
+        int frameX = 0; // third frame (0-based)
+        int frameY = 0; // second row
+
+        sf::IntRect speciesFrameRect(frameX * frameWidth, frameY * frameHeight, frameWidth, frameHeight);
+
+        // assign texture and rect to the sprite (ensure the texture outlives the sprite in real code)
+        sprite.setTexture(pawn_spritesheet);
+        sprite.setTextureRect(speciesFrameRect);
+
+    }
+
+    void Units_Renderer::scale (float scale_x, float scale_y) {
+        this->sprite.setScale(scale_x, scale_y);
+    }
+    void Units_Renderer::render (sf::RenderWindow& window, sf::Vector2f position) {
+        this->sprite.setPosition(position);
+        window.draw(this->sprite);
+    }
+
+};
---
+++ test/shared/test_units_renderer.cpp
@@ -0,0 +1,40 @@
+#include <boost/test/unit_test.hpp>
+
+
+#include <state.h>
+#include <renderer.h>
+
+
+#include <SFML/Graphics.hpp>
+
+BOOST_AUTO_TEST_CASE(TestStaticAssert)
+{
+  BOOST_CHECK(1);
+}
+
+BOOST_AUTO_TEST_CASE(TestTribeRenderer)
+{
+  {
+
+    sf::RenderWindow window(sf::VideoMode(800,600), "Renderer Test");
+    renderer::Units_Renderer units_renderer;
+    units_renderer.set_sprite("Dwarves");
+    units_renderer.scale(1.0f, 1.0f);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        units_renderer.render(window, sf::Vector2f(200,100));
+        window.display();
+    }
+
+
+  }
+}
