a47b5a209518a896bba9f562e49aef36dc3f24af
Maël Archenault <mael.archenault@gmail.com>
2025-10-27 19:16:15 +0100

    Adding auto indexing for units renderer

--- src/shared/renderer/Units_Renderer.cpp
+++ src/shared/renderer/Units_Renderer.cpp
@@ -1,19 +1,57 @@
 #include "Units_Renderer.h"
-
+#include <unordered_map>
+#include <fstream>
+#include <json/json.h>
 
 namespace renderer {
 
     bool Units_Renderer::pawn_spritesheet_loaded = false;
-
+    
     sf::Texture Units_Renderer::pawn_spritesheet;
-   
+    std::unordered_map<std::string, std::pair<int, int>> Units_Renderer::spritesheet_indexing;
+    
     Units_Renderer::Units_Renderer(){
+
+        // loading the spritesheet and its indexing
+        if (!pawn_spritesheet_loaded) {
+            if (!pawn_spritesheet.loadFromFile("/home/mael-archenault/Desktop/Smallworld/src/graphic_resources/pawn_spritesheet_150x150.png")) {
+                throw std::runtime_error("Units_Renderer::set_species: Failed to load pawn spritesheet");
+            }
+            pawn_spritesheet_loaded = true;
+
+           // loading json that contains the indexing
+
+            std::string file_name = "/home/mael-archenault/Desktop/Smallworld/src/graphic_resources/pawn_spritesheet_indexing.json";
+            std::ifstream file(file_name, std::ifstream::binary);
+            if (!file.is_open()) {
+                throw std::runtime_error("Cannot open file: " + file_name);
+            }
+
+            Json::Value root;
+            file >> root;
+            file.close();
+
+            const Json::Value& size = root["sprite_size"];
+            const Json::Value& frames  = root["frames"];
+
+            for (const auto& species_name : frames.getMemberNames()) {
+                std::string name = species_name;
+                int frame_x = frames[species_name][0].asInt();
+                int frame_y = frames[species_name][1].asInt();
+                spritesheet_indexing[name] = std::make_pair(frame_x, frame_y);
+            }
+
+        }
+
+
+
+
         set_sprite("lost_tribe");
         if (!font.loadFromFile("/home/mael-archenault/Desktop/Smallworld/src/graphic_resources/arial.ttf")) {
             throw std::runtime_error("Units_Renderer::Units_Renderer: Failed to load font");
         }
         number.setFont(font);
-        number.setCharacterSize(72); // in pixels, not points!
+        number.setCharacterSize(64); // in pixels, not points!
         number.setFillColor(sf::Color::White);
         number.setStyle(sf::Text::Bold);
 
@@ -22,18 +60,12 @@ namespace renderer {
 
 
     void Units_Renderer::set_sprite (std::string species_name) {
-        if (!pawn_spritesheet_loaded) {
-            if (!pawn_spritesheet.loadFromFile("/home/mael-archenault/Desktop/Smallworld/src/graphic_resources/pawn_spritesheet_255x255.png")) {
-                throw std::runtime_error("Units_Renderer::set_species: Failed to load pawn spritesheet");
-            }
-            pawn_spritesheet_loaded = true;
-        }
 
-        int frameWidth = 255;
-        int frameHeight = 255;
+        int frameWidth = 150;
+        int frameHeight = 150;
 
-        int frameX = 0; // third frame (0-based)
-        int frameY = 0; // second row
+        int frameX = spritesheet_indexing[species_name].second;
+        int frameY = spritesheet_indexing[species_name].first;
 
         sf::IntRect speciesFrameRect(frameX * frameWidth, frameY * frameHeight, frameWidth, frameHeight);
 
