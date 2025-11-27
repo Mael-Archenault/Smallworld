9e11ae72bcf0a24b85dccbad1e34dee0366cf9f0
ViMorel <victormorel45190@gmail.com>
2025-11-27 11:21:49 +0100

    updated

--- src/shared/state/Trolls_Species.cpp
+++ src/shared/state/Trolls_Species.cpp
@@ -6,6 +6,20 @@ using namespace state;
 Trolls_Species::Trolls_Species(): Species_Description("Trolls",5,10){}
 
 void Trolls_Species::apply_additional_defense(Area* area){
+    for(Area_Special_Token specials_tokens : area->get_special_tokens()){
+        if(specials_tokens==TROLL_LAIR){
+            return;
+        }
+    }
     area->set_special_tokens(TROLL_LAIR);
-    return;
+}
+
+void Trolls_Species::disable_effects(std::vector<Area*> areas){
+    for(Area* area : areas){
+        area->get_special_tokens().erase(
+            std::remove(area->get_special_tokens().begin(),
+                area->get_special_tokens().end(),
+                TROLL_LAIR),
+                area->get_special_tokens().end());
+    }
 }
