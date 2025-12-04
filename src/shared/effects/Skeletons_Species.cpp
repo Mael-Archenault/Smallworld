b73628c823786b8dcc51f39bd38fdceb43bc3ddb
Maël Archenault <mael.archenault@gmail.com>
2025-12-04 19:36:54 +0100

    adding skeletons

--- src/shared/effects/Skeletons_Species.cpp
+++ src/shared/effects/Skeletons_Species.cpp
@@ -8,25 +8,18 @@ namespace effects
 
 Skeletons_Species::Skeletons_Species() : Species_Description("Skeletons", 6, 20)
 {
+    max_units_number          = 20;
     non_empty_areas_conquered = 0;
 }
 
-int Skeletons_Species::add_free_units(int current_units_number)
+int Skeletons_Species::second_gather_effect(int initial_free_units_number)
 {
     int additional_free_units = non_empty_areas_conquered / 2;
     non_empty_areas_conquered = 0;
-    if (current_units_number + additional_free_units <= max_units_number)
-    {
-        return additional_free_units;
-    }
-    if (max_units_number > current_units_number)
-    {
-        return max_units_number - current_units_number;
-    }
-    return 0;
+    return std::min(initial_free_units_number + additional_free_units, max_units_number);
 }
 
-void Skeletons_Species::apply_conquest_effect(state::Area* attacked_area)
+void Skeletons_Species::conquest_effect(state::Area* attacked_area)
 {
     if (attacked_area->get_units_number() != 0)
     {
