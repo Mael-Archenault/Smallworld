78a581d5edbecd0b49f26690e8721d82b9ddf4b3
julien <julien@metzelard.net>
2025-12-04 11:43:16 +0100

    [Fix] when both player went to decline at the same time.

--- src/shared/engine/Redeploy_Command.cpp
+++ src/shared/engine/Redeploy_Command.cpp
@@ -20,9 +20,12 @@ Redeploy_Command::Redeploy_Command(int player_id, int area_id, int added_units)
 
 void Redeploy_Command::execute(state::Game_State& state)
 {
-    state.redeploy_units(player_id, area_id, added_units);
-    std::cout << state.get_free_units_number(player_id) << " free units remaining for player "
-              << player_id << std::endl;
+    if (state.get_free_units_number(player_id) != 0) {
+        state.redeploy_units(player_id, area_id, added_units);
+        std::cout << state.get_free_units_number(player_id) << " free units remaining for player "
+                  << player_id << std::endl;
+    }
+
     if (state.get_free_units_number(player_id) == 0)
     {
         state.get_rewards(player_id);
